be384758b410ede3d4c349b86e75afffc769c94e
Gavin Sanchez <mark_gavin_sanchez@dlsu.edu.ph>
2017-12-08 16:19:31 +0800

    trying pool

--- gavinsamp.js
+++ gavinsamp.js
@@ -8,7 +8,27 @@ var node1 = mysql.createConnection({
   
 });
 
+var mysql      = require('mysql');
+var node2 = mysql.createConnection({
+  host     : '172.20.10.14',
+  localAddress: '172.20.10.14',
+  user     : 'root',
+  password : 'password',
+  database : 'college_admissions'
+  
+});
 
+var mysql      = require('mysql');
+var node3 = mysql.createConnection({
+  host     : '172.20.10.14',
+  localAddress: '172.20.10.14',
+  user     : 'root',
+  password : 'password',
+  database : 'college_admissions'
+  
+});
+
+/*
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -31,3 +51,13 @@ connection.query('UPDATE student set name = "Gavin" where studID = "stud1"' , fu
     if (error) throw error;
     console.log('The solution is: ', results);
   });
+  */
+  
+  // create
+var poolCluster = mysql.createPoolCluster();
+
+// add configurations (the config is a pool config object)
+poolCluster.add(config); // add configuration with automatic name
+poolCluster.add('MASTER', masterConfig); // add a named configuration
+poolCluster.add('SLAVE1', slave1Config);
+poolCluster.add('SLAVE2', slave2Config);
