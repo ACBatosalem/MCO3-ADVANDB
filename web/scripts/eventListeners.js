fe54d95d8e620072dc0e8dce19f0ec2873fb05d1
Troy Mirafuentes <troy_mirafuentes@dlsu.edu.ph>
2017-12-11 02:10:24 +0800

    Query Making Possible but with Problem

--- web/scripts/eventListeners.js
+++ web/scripts/eventListeners.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
     $("#case2_selectDiv").removeClass("action_selected");
     $("#case3_insertDiv").removeClass("action_selected");
     
+    var ctr1 = 0, ctr2 = 0, ctr3 = 0;
+    
     // Listen to change
     /*for (var i=1;i<4;i++) {
         var identifier = "#case" + i + "_action_dropdown";
@@ -50,9 +52,13 @@ $(document).ready(function() {
     });
     
     $("#case2_action_dropdown").change(function() {
+        //$(".case2_filters").remove();
+        //$(".case2_filterAdd").remove();
         var val = $("#case2_action_dropdown").val();
         switch(val) {
             case "SELECT ":
+                ctr2 = 0;
+                //addFilterDiv(2, val);
                 $(".case2_filters").empty();
                 $(".case2_actions").addClass("action_selected");
                 $("#case2_selectDiv").removeClass("action_selected");
@@ -63,11 +69,15 @@ $(document).ready(function() {
                 $("#case2_insertDiv").removeClass("action_selected");
                 break;
             case "UPDATE ":
+                ctr2 = 0;
+                //addFilterDiv(2, val);
                 $(".case2_filters").empty();
                 $(".case2_actions").addClass("action_selected");
                 $("#case2_updateDiv").removeClass("action_selected");
                 break;  
             case "DELETE FROM ":
+                ctr2 = 0;
+                //addFilterDiv(2, val);
                 $(".case2_filters").empty();
                 $(".case2_actions").addClass("action_selected");
                 $("#case2_deleteDiv").removeClass("action_selected");
@@ -76,6 +86,8 @@ $(document).ready(function() {
     });
     
     $("#case3_action_dropdown").change(function() {
+        //$(".case3_filters").remove();
+        //$(".case3_filterAdd").remove();
         var val = $("#case3_action_dropdown").val();
         switch(val) {
             case "INSERT INTO ":
@@ -84,11 +96,15 @@ $(document).ready(function() {
                 $("#case3_insertDiv").removeClass("action_selected");
                 break;
             case "UPDATE ":
+                ctr3 = 0;
+                //addFilterDiv(3, val);
                 $(".case3_filters").empty();
                 $(".case3_actions").addClass("action_selected");
                 $("#case3_updateDiv").removeClass("action_selected");
                 break;  
             case "DELETE FROM ":
+                ctr3 = 0;
+                //addFilterDiv(3, val);
                 $(".case3_filters").empty();
                 $(".case3_actions").addClass("action_selected");
                 $("#case3_deleteDiv").removeClass("action_selected");
@@ -96,78 +112,220 @@ $(document).ready(function() {
         }
     });
     
+    function addFilterDiv(caseNum, actionType) {
+        var divCode = "<input type='button' class='case" + caseNum + "_filterAdd name='case" + caseNum + "_filterAdd' value='Add Filtering Option' colName=<%=columnNames%>' op='<%=operators%>'><div class='filter_options case" + caseNum + "_filters'></div>";
+        if (actionType === "SELECT ")
+            $("#case" + caseNum + "_selectDiv").append(divCode);
+        else if (actionType === "UPDATE ")
+            $("#case" + caseNum + "_updateDiv").append(divCode);
+        else if (actionType === "DELETE FROM ")
+            $("#case" + caseNum + "_deleteDiv").append(divCode);
+    }
+    
+    function buttonListener(caseNumber, actionType, counter) {
+        counter++;
+        var columnNames = $(".case" + caseNumber + "_filterAdd_" + actionType).attr('colName').split(",");
+        var operators = $(".case" + caseNumber + "_filterAdd_" + actionType).attr('op').split(",");
+
+        var append = "<select class='case" + caseNumber + "_fields_" + actionType + " case" + caseNumber + "_field" + counter + "'>";
+        for(var j=0;j<columnNames.length;j++)
+            append += "<option value='" + operators[j] + "'>" + columnNames[j] + "</option>";
+        append += "</select><select class='case" + caseNumber + "_operator" + counter + " case" + caseNumber + "_operators_" + actionType + "'>";
+        for(var i=0;i<operators.length;i++)
+            append += "<option value='" + operators[i] + "'>" + operators[i] + "</option>"; 
+         append += "</select><input type='text' class='case" + caseNumber + "_filter" + counter + "_text_" + actionType + "'><br><br>";
+
+        //console.log(".case" + caseNumber + "_filters_" + actionType);
+        $(".case" + caseNumber + "_filters_" + actionType).append(append);
+        return counter;
+    }
+    
+    $(".case1_filterAdd_select").click(function() {
+       ctr1 = buttonListener(1, "select", ctr1);
+    });
+    $(".case2_filterAdd_select").click(function() {
+       ctr2 = buttonListener(2, "select", ctr2); 
+    });
+    $(".case2_filterAdd_update").click(function() {
+       ctr2 = buttonListener(2, "update", ctr2); 
+    });
+    $(".case2_filterAdd_delete").click(function() {
+       ctr2 = buttonListener(2, "delete", ctr2); 
+    });
+    $(".case3_filterAdd_update").click(function() {
+       ctr3 = buttonListener(3, "update", ctr3); 
+    });
+    $(".case3_filterAdd_delete").click(function() {
+       ctr3 = buttonListener(3, "delete", ctr3); 
+    });
+    
     // Filtering Option Listener
-    var ctr1 = 0;
-    $(".case1_filterAdd").click(function(){
+    /*$(".case1_filterAdd_select").click(function(){
         ctr1++;
         
-        var columnNames = $(".case1_filterAdd").attr('colName').split(",");
-        var operators = $(".case1_filterAdd").attr('op').split(",");
+        var columnNames = $(".case1_filterAdd_select").attr('colName').split(",");
+        var operators = $(".case1_filterAdd_select").attr('op').split(",");
         
-        var append1 = "<select id='case1_filter" + ctr1 + "'>";
+        var append1 = "<select class='case1_fields case1_field" + ctr1 + "'>";
         for(var j=0;j<columnNames.length;j++) { 
             append1 += "<option value='"+columnNames[j]+"'>"+columnNames[j]+"</option>"; 
         } 
-        append1 += "</select><select id='case1_operator" + ctr1 + "'>";
+        append1 += "</select><select class='case1_operator" + ctr1 + "'>";
         for(var i=0;i<operators.length;i++) { 
             append1 += "<option value='"+operators[i]+"'>"+operators[i]+"</option>";
          } 
-         append1 += "</select><input type='text' id='case1_filter" + ctr1 + "_text'><br><br>";
+         append1 += "</select><input type='text' class='case1_filter" + ctr1 + "_text'><br><br>";
 
-        $(".case1_filters").append(append1);
+        $(".case1_filters_select").append(append1);
     });
-    
-    var ctr2 = 0;
+
     $(".case2_filterAdd").click(function(){
         ctr2++;
         
         var columnNames = $(".case2_filterAdd").attr('colName').split(",");
         var operators = $(".case2_filterAdd").attr('op').split(",");
         
-        var append2 = "<select id='case2_filter" + ctr2 + "'>";
+        var append2 = "<select class='case2_fields case2_field" + ctr2 + "'>";
         for(var j=0;j<columnNames.length;j++) { 
             append2 += "<option value='"+columnNames[j]+"'>"+columnNames[j]+"</option>"; 
         } 
-        append2 += "</select><select id='case2_operator" + ctr2 + "'>";
+        append2 += "</select><select class='case2_operator" + ctr2 + "'>";
         for(var i=0;i<operators.length;i++) { 
             append2 += "<option value='"+operators[i]+"'>"+operators[i]+"</option>";
          } 
-         append2 += "</select><input type='text' id='case2_filter" + ctr2 + "_text'><br><br>";
+         append2 += "</select><input type='text' class='case2_filter" + ctr2 + "_text'><br><br>";
 
         $(".case2_filters").append(append2);
     });
     
-    var ctr3 = 0;
     $(".case3_filterAdd").click(function(){
         ctr3++;
         var columnNames = $(".case3_filterAdd").attr('colName').split(",");
         
         var operators = $(".case3_filterAdd").attr('op').split(",");
         
-        var append3 = "<select id='case3_filter" + ctr3 + "'>";
+        var append3 = "<select  class='case3_fields case3_field" + ctr3 + "'>";
         for(var j=0;j<columnNames.length;j++) { 
             append3 += "<option value='"+columnNames[j]+"'>"+columnNames[j]+"</option>"; 
         } 
-        append3 += "</select><select id='case3_operator" + ctr3 + "'>";
+        append3 += "</select><select class='case3_operator" + ctr3 + "'>";
         for(var i=0;i<operators.length;i++) { 
             append3 += "<option value='"+operators[i]+"'>"+operators[i]+"</option>";
          } 
-         append3 += "</select><input type='text' id='case3_filter" + ctr3 + "_text'><br><br>";
+         append3 += "</select><input type='text' class='case3_filter" + ctr3 + "_text'><br><br>";
 
         $(".case3_filters").append(append3);
-    });
+    });*/
     
     // Submit Query
     $("#submitQueryForm").submit(function(e) {
         e.preventDefault();
         
+        var dbFields = ["ID", "CountryName", "CountryRegion", "CountryIncome", "SeriesName", "Year", "Data"];
+        
+        var $allCases = $("input[name=transaction_checkbox]");
         var $runCases = $("input[name=transaction_checkbox]:checked");
         if ($runCases.length == 0) {
             $("#console_textarea").text("You need to check one of the cases before running.");
             return false;
         } 
         else {
-            var case1 = $("#trans_chkBox_1").val();
+            var caseQuery = '';
+            $allCases.each(function(index, element) {
+                if ($(element).is(":checked")) {
+                    var caseNumber = index + 1;
+                
+                    var actionType = $("#case" + caseNumber + "_action_dropdown").val();
+                    caseQuery += actionType;
+                    switch(actionType) {
+                        case "SELECT ":
+                            caseQuery += "* ";
+                            caseQuery += "FROM " + $("#case" + caseNumber + "_area_dropdown").val();
+                            
+                            console.log("select[name=case" + caseNumber + "_fields_select]");
+                            var $allFilters = $("select[name=case" + caseNumber + "_fields_select]");
+                            if ($allFilters.length > 0) {
+                                caseQuery += " WHERE ";
+                                var multi = false;
+                                $allFilters.each(function(ind, el) {
+                                    var caseCtr = ind + 1;
+                                    if ($(".case" + caseNumber + "_filter" + caseCtr + "_text_select").val() > 0) {
+                                        if (multi)
+                                            caseQuery += " AND ";
+                                        caseQuery += $(el).val() + " " + $(".case" + caseNumber + "_operators_select").val() + " '" + $(".case" + caseNumber + "_filter" + caseCtr + "_text_select").val() + "'";
+                                        multi = true;
+                                    }
+                                });
+                            }
+                            else {
+                                console.log($allFilters.length);
+                            }
+                            break;
+                        case "INSERT INTO ":
+                            caseQuery += $("#case" + caseNumber + "_area_dropdown").val() + " VALUES (";
+                            for (var k=1;k<dbFields.length;k++) {
+                                caseQuery += "'" + $("#case" + caseNumber + "_insert_" + dbFields[k]) + "'";
+                                if (dbFields.length - 1 != k)
+                                    caseQuery += ", ";
+                            }
+                            caseQuery += ")";
+                            break;
+                        case "UPDATE ":
+                            var multiple = false;
+                            caseQuery += $("#case" + caseNumber + "_area_dropdown").val() + " SET ";
+                            for (var k=1;k<dbFields.length;k++) {
+                                if ($("#case" + caseNumber + "_update_" + dbFields[k]).val().length > 0) {
+                                    if (multiple)
+                                        caseQuery += ", ";
+                                    caseQuery += dbFields[k] + " = '" + $("#case" + caseNumber + "_update_" + dbFields[k]).val() + "'";
+                                    multiple = true;
+                                }
+                            }
+                            
+                            var $allFilters = $("select[class=case" + caseNumber + "_fields_update]");
+                            if ($allFilters.length > 0) {
+                                caseQuery += " WHERE ";
+                                var multi = false;
+                                $allFilters.each(function(ind, el) {
+                                    var caseCtr = ind + 1;
+                                    
+                                    if ($(".case" + caseNumber + "_filter" + caseCtr + "_text_update").val() > 0) {
+                                        if (multi)
+                                            caseQuery += " AND ";
+                                        caseQuery += $(el).val() + " " + $(".case" + caseNumber + "_operators_update").val() + " '" + $(".case" + caseNumber + "_filter" + caseCtr + "_text_update").val() + "'";
+                                        multi = true;
+                                    }
+                                });
+                            }
+                            break;
+                        case "DELETE FROM ":
+                            caseQuery += $("#case" + caseNumber + "_area_dropdown").val();
+                            
+                            var $allFilters = $("select[class=case" + caseNumber + "_fields_delete]");
+                            if ($allFilters.length > 0) {
+                                caseQuery += " WHERE ";
+                                var multi = false;
+                                $allFilters.each(function(ind, el) {
+                                    var caseCtr = ind + 1;
+                                    if ($(".case" + caseNumber + "_filter" + caseCtr + "_text_delete").val() > 0) {
+                                        if (multi)
+                                            caseQuery += " AND ";
+                                        caseQuery += $(el).val() + " " + $(".case" + caseNumber + "_operators_delete").val() + " '" + $(".case" + caseNumber + "_filter" + caseCtr + "_text_delete").val() + "'";
+                                        multi = true;
+                                    }
+                                });
+                            }
+                            break;
+                    }
+                    caseNumber++;
+                    caseQuery += "; ";
+                }
+            });
+            $("#newQuery").val(caseQuery);
+            console.log(caseQuery);
+            //this.submit();
+            
+            /*var case1 = $("#trans_chkBox_1").val();
             if ($("#trans_chkBox_1").is(":checked")) {
                 var case1Query = '';
 
@@ -176,19 +334,9 @@ $(document).ready(function() {
                 $("#newQuery").val(case1Query);
                 console.log($("#newQuery").val());
                 this.submit();
-               /* $.ajax({
-                    type        : 'POST',
-                    url         : 'submitQuery',
-                    data        : {newQuery: case1Query},
-                    dataType    : 'html',
-                    success     : function(data) {
-                                    console.log(data);
-                                    $("#console_textarea").text();
-                                    $("#console_textarea").text(data);
-                                  }
-                });*/
                 return true;
-            }
+            }*/
+            //if ($("#"))
         }
     });
 });
